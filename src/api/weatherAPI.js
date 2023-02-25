cc7d9c2eb073fde252653629e2f44b4cee09f150
Alejandro <hansalejandroca@gmail.com>
2023-02-25 13:24:46 -0500

    first commit

---
+++ src/api/weatherAPI.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "7b179e3fa7mshb30324fddd87ec7p124b63jsn9ad19cb8f794",
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
---
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from "./style.module.css";
+
+const CurrentWeather = () => {
+  return (
+    <div>CurrentWeather</div>
+  )
+}
+
+export default CurrentWeather
